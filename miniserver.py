2510e0337e295a1f5015c4c38f8270496a7bee31
deniscyril <denis.prof@gmail.com>
2014-04-03 17:23:17 +0200

    MAJ miniserveur

--- miniserver.py
+++ miniserver.py
@@ -3,25 +3,20 @@ from bottle import request, route, run
 @route("/", method="GET")
 def hello():
 	name = request.query.get("name")
-# on génère la page html ici
 	out = "<form method='POST' action=''name='tablo'><table>"
-	for i in range(1,10):
-		out += "<tr>"
-		for j in range(1,10):
-			out += "<td><input name='row"+str(i)+'-'+str(j)+"' type='text'/></td>"
-		out += "</tr>"
+	out += "<td><input name='case1' type='text'/></td>"
+	out += "</tr>"
 	out += "</table>"
 	out += "<input type='submit' value='Envoyer'/></form><hr/>"
-# on retourne la page html
 	return out
 
 @route("/", method="POST")
+	def reponse():
+		rep  = request.forms.get("case1")
+		return """
+	<h1>Votre choix : %s """ % (rep,)
 
 
-def reponse():
-	rep  = request.forms.get("row11")
-	return """
-<h1>Votre choix : %s """ % (rep,)
 
-# Toto
-run(host="localhost", port=8123, reloader=True)
+# mise en route du serveur
+run(host="localhost", port=8000, reloader=True)
