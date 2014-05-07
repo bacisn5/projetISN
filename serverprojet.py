b3ac4901c84e18eeabb2dc5884ee2c7091cc23c5
nicolasculerier <nicolas.culerier@laposte.net>
2014-05-07 14:36:37 +0200

    Quelques modifs syntaxiques et tri dans les fichiers. Le fichier style.css est le fichier css2actions renommé. J'ai copié le codage de serveur2actions dans serverprojet et j'ai renommé formulaire2actions formulaire et supprimer tous les fichiers inutiles
    Signed-off-by: nicolasculerier <nicolas.culerier@laposte.net>

--- serverprojet.py
+++ serverprojet.py
@@ -1,8 +1,27 @@
+# -*- coding: utf-8 -*-
 from bottle import request, route, run, template, get
 
+@route("/", method="GET")
+def affiche():
+    return template("formulaire.html")
+
 @route("/action1", method="GET")
+
 def action1(): 
-    concentration=request.query.get("cm")
-    return "OK"
+    concentration=float(request.query.get("cm"))
+    concentration1=float(request.query.get("cf"))
+    volume=float(request.query.get("vf"))
+    return "Volume a prelever vaut :"+str(concentration*volume/concentration1)
+
+@ route("/action2",method="GET")
+
+def action2():
+    concentrationmol=float(request.query.get("cmol"))
+    massemol=float(request.query.get("mmol"))
+    volume=float(request.query.get("vol"))
+    return "Masse a prelever vaut :"+str(concentrationmol*massemol*volume)
+#template("fonctiondilution.py ")
+# elif :
+#    return "Vous n'avez pas tout compléter"
 # mise en route du serveur
 run(host="localhost", port=8000, reloader=True)
