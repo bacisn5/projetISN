def4ea1ab7fbcb63f9ff3a45b78a7152db1f988b
nicolasculerier <nicolas.culerier@laposte.net>
2014-05-22 15:19:52 +0200

    Adaptation du codage aux noms des fichiers de sortie
    Signed-off-by: nicolasculerier <nicolas.culerier@laposte.net>

--- serverprojet.py
+++ serverprojet.py
@@ -21,7 +21,7 @@ def action1():
     concentration=float(request.query.get("cm"))
     concentration1=float(request.query.get("cf"))
     volume=float(request.query.get("vf"))
-    return template("sortie.tpl",{'vm': str(concentration*volume/concentration1)})
+    return template("sortiedilution.tpl",{'vm': str(concentration*volume/concentration1)})
 
 @ route("/action2",method="GET")
 
@@ -29,7 +29,7 @@ def action2():
     concentrationmol=float(request.query.get("cmol"))
     massemol=float(request.query.get("mmol"))
     volume=float(request.query.get("vol"))
-    return template("sortie.tpl",{"La masse à prelever vaut :"+str(concentrationmol*massemol*volume)})
+    return template("sortiemasse.tpl",{'m': str(concentrationmol*massemol*volume)})
 
 # mise en route du serveur
 run(host="localhost", port=8000, reloader=True)
