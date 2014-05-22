f24d901bca6fa211ccc1a911cde33e286e8ca77b
nicolasculerier <nicolas.culerier@laposte.net>
2014-05-22 14:43:22 +0200

    Modification code calcul dilution pour fichier sortie
    Signed-off-by: nicolasculerier <nicolas.culerier@laposte.net>

--- serverprojet.py
+++ serverprojet.py
@@ -4,6 +4,7 @@ from bottle import request, route, run, template, get, static_file
 @route("/", method="GET")
 def affiche():
     return template("formulaire.html")
+
 from bottle import static_file
 
 @route('/css/<filename>')
@@ -20,7 +21,7 @@ def action1():
     concentration=float(request.query.get("cm"))
     concentration1=float(request.query.get("cf"))
     volume=float(request.query.get("vf"))
-    return template("sortie.tpl",{"Le volume à prelever vaut :"+str(concentration*volume/concentration1)})
+    return template("sortie.tpl",{'vm': str(concentration*volume/concentration1)})
 
 @ route("/action2",method="GET")
 
